7104eb9247e2851f81b3bbab283004cc81e2b304
dhanparkash <114330505+dhanparkash@users.noreply.github.com>
2025-12-29 16:10:16 +0530

    post.html added

--- app.py
+++ app.py
@@ -1,65 +1,34 @@
-from flask import Flask, request, render_template_string
-import smtplib
-from email.message import EmailMessage
+from flask import Flask, render_template, abort
 
 app = Flask(__name__)
 
-# HTML form
-form_html = '''
-<h2>Student Grade Form</h2>
-<form method="post">
-  Name: <input type="text" name="name" required><br><br>
-  Email: <input type="email" name="email" required><br><br>
-  Marks: <input type="number" name="marks" required><br><br>
-  <input type="submit">
-</form>
-'''
+# Fake blog data
+posts = [
+    {
+        "id": 1,
+        "title": "My First Blog Post",
+        "content": "This is my first blog post using Flask."
+    },
+    {
+        "id": 2,
+        "title": "Learning Flask",
+        "content": "Flask is simple and powerful for small projects."
+    }
+]
+@app.route("/")
+def home():
+    return render_template("home.html")
 
-# Email credentials
-EMAIL = "yash.dhiman@girlpowertalk.com"
-PASSWORD = "pqgd rmbr bjjd fkqt"  # Gmail App Password
+@app.route("/blog")
+def blog():
+    return render_template("blog.html", posts=posts)
 
-def send_email(name, email, marks, grade):
-    msg = EmailMessage()
-    msg["Subject"] = "Student Grade Result"
-    msg["From"] = EMAIL
-    msg["To"] = email
-    msg.set_content(f"""
-Student Name: {name}
-Student Email: {email}
-Marks: {marks}
-Grade: {grade}
-""")
-
-    # Send email
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-        server.login(EMAIL, PASSWORD)
-        server.send_message(msg)
-
-@app.route("/", methods=["GET", "POST"])
-def grade_form():
-    if request.method == "POST":
-        name = request.form["name"]
-        email = request.form["email"]
-        marks = int(request.form["marks"])
-
-        # Grade logic
-        if marks >= 90:
-            grade = "A"
-        elif marks >= 80:
-            grade = "B"
-        elif marks >= 70:
-            grade = "C"
-        elif marks >= 50:
-            grade = "D"
-        else:
-            grade = "F"
-
-        send_email(name, email ,marks, grade)
-
-        return "<h3>Email sent successfully!</h3>"
-
-    return render_template_string(form_html)
+@app.route("/blog/<int:post_id>")
+def post(post_id):
+    post = next((p for p in posts if p["id"] == post_id), None)
+    if not post:
+        abort(404)
+    return render_template("post.html", post=post)
 
 if __name__ == "__main__":
     app.run(debug=True)

3d7d67e53b327a8dbee8275ab674640dd9babd61
dhanparkash <114330505+dhanparkash@users.noreply.github.com>
2025-12-30 18:32:46 +0530

    blog page update and single blog page also

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 from flask import Flask, render_template, abort
 from datetime import datetime
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 # Fake blog data
 posts = [
     {
         "id": 1,
+        "slug": "my-first-blog",
         "title": "My First Blog Post",
-        "content": "This is my first blog post using Flask."
+        "post_thumb": "images/finance.webp",
+        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
     },
     {
         "id": 2,
+        "slug": "my-second-blog",
+        "title": "My Second Blog Post",
+                "post_thumb": "images/contact-us.jpg",
+        "content": "This is my second blog post using Flask."
+    },
+    {
+        "id": 3,
+        "slug": "my-third-blog",
+        "title": "My Third Blog Post",
+                "post_thumb": "images/images.jpg",
+        "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+    {
+        "id": 4,
+        "slug": "my-forth-blog",
         "title": "Learning Flask",
+                "post_thumb": "images/mobile.png",
         "content": "Flask is simple and powerful for small projects."
     }
 ]
@@ -25,9 +43,9 @@ def home():
 def blog():
     return render_template("blog.html", posts=posts)
 
-@app.route("/blog/<int:post_id>")
-def post(post_id):
-    post = next((p for p in posts if p["id"] == post_id), None)
+@app.route("/blog/<slug>")
+def post(slug):
+    post = next((p for p in posts if p["slug"] == slug), None)
     if not post:
         abort(404)
     return render_template("post.html", post=post)

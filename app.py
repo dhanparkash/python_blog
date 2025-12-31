f425b5c6e6cd77e2d42df66e083a581826d0f909
Yash Dhiman <114330505+dhanparkash@users.noreply.github.com>
2025-12-31 18:32:19 +0530

    Update app.py
    added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
 from admin.routes import admin_bp, init_db, create_admin
 from main_app.routes import main_bp
 import os
+from flask import Flask
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "supersecretkey")
@@ -10,9 +10,9 @@ app.secret_key = os.environ.get("SECRET_KEY", "supersecretkey")
 init_db()
 create_admin()
 
-# Register Blueprints
-app.register_blueprint(admin_bp, url_prefix="/admin")
-app.register_blueprint(main_bp, url_prefix="")  # front-end
+# Register blueprints
+app.register_blueprint(admin_bp, url_prefix="/admin")  # admin routes
+app.register_blueprint(main_bp, url_prefix="")        # front-end
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
